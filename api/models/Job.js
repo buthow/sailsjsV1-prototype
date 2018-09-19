fb9fdb3a2477e33e39998ac80fcf4d2817e5395f
Benny Ng <bennyng@jobstore.com>
2018-09-19 17:13:22 +0800

    Init Repo

---
+++ api/models/Job.js
@@ -0,0 +1,55 @@
+/**
+ * Job.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    title: {
+      type: 'string',
+      required: true,
+      maxLength: 200,
+      example: 'Software Engineer'
+    },
+
+    location: {
+      type: 'string',
+      description: `Job Location`,
+      example: 'Malaysia, Kuala Lumpur'
+    },
+
+    description: {
+      type: 'string',
+      required: true,
+      description: `Job Descriptions`,
+      maxLength: 120,
+      example: 'Lisa Microwave van der Jenny'
+    },
+
+    status: {
+      type: 'number',
+      description: 'Status to determine Job is active/inactive',
+      example: 1502844074211
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
